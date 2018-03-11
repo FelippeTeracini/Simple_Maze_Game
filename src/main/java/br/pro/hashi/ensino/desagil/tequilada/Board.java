cb9146c1b808ab6cb3bd4dd45a96f578b6080f1a
TeraByte900 <fe.n.teracini@hotmail.com>
2018-03-11 13:29:11 -0300

    alteracoes

--- src/main/java/br/pro/hashi/ensino/desagil/tequilada/Board.java
+++ src/main/java/br/pro/hashi/ensino/desagil/tequilada/Board.java
@@ -6,58 +6,74 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Board {
-	
-	boolean isWall[][];
-	
-	void loadFromFile(String path){
-		
-		try{
-			
+	private boolean[][] isWall;
+	private int numRows;
+	private int numCols;
+
+
+	public Board(String path) {
+		// A instrução "try" delimita um trecho no qual erros
+		// graves, que devem ser tratados, podem acontecer.
+		try {
+			// Objetos da classe FileReader não possuem o método
+			// readLine, então construímos um objeto da classe
+			// BufferedReader "em volta" para dar essa capacidade.
 			BufferedReader reader = new BufferedReader(new FileReader(path));
-			
+
+			// Lê a primeira linha do arquivo e devolve como String.
 			String line = reader.readLine();
-			
+
+			// Usando espaços em branco como o critério de separação,
+			// quebra a string em pedaços e devolve um vetor de strings.
 			String[] words = line.split(" ");
-			
-			int numRows = Integer.parseInt(words[0]);
-			int numColumns = Integer.parseInt(words[1]);
-			
-			isWall = new boolean[numRows][numColumns];
-			
-			for(int i = 0; i < numRows; i++){
-				
+
+			// Converte cada um dos dois pedaços em um inteiro.
+			numRows = Integer.parseInt(words[0]);
+			numCols = Integer.parseInt(words[1]);
+
+			// Constrói uma matriz, de acordo com o número de colunas
+			// e número de linhas lidos do arquivo, e atribui a isWall.
+			isWall = new boolean[numRows][numCols];
+
+			// Para cada uma das linhas restantes do arquivo...
+			for(int i = 0; i < numRows; i++) {
 				line = reader.readLine();
-				
-				for(int j = 0; j < numColumns; j++){
-					
+
+				// Para cada um dos caracteres da linha...
+				for(int j = 0; j < numCols; j++) {
 					boolean value;
-					
-					if(line.charAt(j) == '#'){
+
+					if(line.charAt(j) == '#') {
 						value = true;
 					}
-					else{
+					else {
 						value = false;
 					}
-					
+
 					isWall[i][j] = value;
 				}
 			}
-			
+
+			// Fecha o reader, pois seu uso terminou.
 			reader.close();
-			
 		}
-		
-		catch(FileNotFoundException exception){
-			
+
+		// Trata o erro FileNotFoundException, se ele acontecer.
+		catch(FileNotFoundException exception) {
 			System.out.println(exception);
-			
 		}
-		
-		catch(IOException exception){
-			
+
+		// Trata o erro IOException, se ele acontecer.
+		catch(IOException exception) {
 			System.out.println(exception);
-			
 		}
 	}
-	
+
+
+	public int getNumRows() {
+		return numRows;
+	}
+	public int getNumCols() {
+		return numCols;
+	}
 }
--- src/main/java/br/pro/hashi/ensino/desagil/tequilada/Player.java
+++ src/main/java/br/pro/hashi/ensino/desagil/tequilada/Player.java
@@ -1,34 +1,23 @@
 package br.pro.hashi.ensino.desagil.tequilada;
 
 public class Player {
-	
 	private int row;
 	private int col;
-	
-	public Player(int row, int col){
-		
+
+	public Player(int row, int col) {
 		this.row = row;
 		this.col = col;
-		
-		
 	}
-	
-	void move(int rowShift, int colShift){
-		
+
+	public void move(int rowShift, int colShift) {
 		row += rowShift;
 		col += colShift;
-		
 	}
-	
-	public int getRow(){
-		
+
+	public int getRow() {
 		return row;
-		
 	}
-	
-	public int getCol(){
-		
+	public int getCol() {
 		return col;
-		
 	}
 }
